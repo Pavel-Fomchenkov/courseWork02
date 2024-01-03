4a53cb9e24d8431cb4b9dcd305505fe034e50492
Pavel-Fomchenkov <donotwritemail@gmail.com>
2024-01-03 17:20:16 +0300

    Initial commit

---
+++ src/main/java/pro/sky/javacoursepart2/courseWork02/CourseWork02Application.java
@@ -0,0 +1,13 @@
+package pro.sky.javacoursepart2.courseWork02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWork02Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/javacoursepart2/courseWork02/CourseWork02ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.javacoursepart2.courseWork02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
